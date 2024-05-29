c635e0ddcd28a8e5d6b1a824e0a34011de658f63
Evangelos Angelakis <Evangelosangel@gmail.com>
2024-05-29 08:44:21 +0100

    Added Buttons to toggle Text to bold and change background colour

---
+++ Personal Info Page.js
@@ -0,0 +1,10 @@
+function toggleBold() {
+const el = 
+document.querySelector("body");
+el.classList.toggle("bold");
+}
+function toggleBackgroundColor() {
+    const el = 
+    document.querySelector("body");
+    el.classList.toggle("BackGroundColor");
+}
